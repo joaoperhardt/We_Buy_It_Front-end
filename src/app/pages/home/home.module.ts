e34570e2ca5c2d5fb9001c003db8587ad2357f33
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2024-02-20 19:17:01 -0300

    add home page and header module

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { HeaderModule } from './header/header.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HeaderModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class HomeModule { }
