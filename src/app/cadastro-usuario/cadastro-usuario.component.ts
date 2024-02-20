5e2d6ce7d76b1fc387eab7e6bad67dbe32cfebf5
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-02-20 19:50:35 -0300

    componente cadastro usuário criado

---
+++ src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-usuario.component.html',
+  styleUrl: './cadastro-usuario.component.scss'
+})
+export class CadastroUsuarioComponent {
+
+}
