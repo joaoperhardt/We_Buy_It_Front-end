2f22aadb6b338b37a00e0d55355071809540a6c0
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2024-02-20 21:40:44 -0300

    add user profile ts and backend functions

--- src/app/backend/backend.service.ts
+++ src/app/backend/backend.service.ts
@@ -8,4 +8,18 @@ import { Observable } from 'rxjs';
 export class BackendService {
 
   constructor(private http: HttpClient) { }
+
+  public getByIdUser(id: string):Observable<any> {
+    const route: string = `https://localhost:7191/user/${id}`;
+    return this.http.get(route, {
+      
+    });
+  }
+
+  public update(userData: any):Observable<any> {
+    const route:string = 'https://localhost:7191/user';
+    return this.http.put(route, userData, {
+      
+    });
+  }
 }
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { BackendService } from 'src/app/backend/backend.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload'; 
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,7 +11,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent {
-  constructor(library: LaIconLibrary) {
-    library.addIcons([lasSearch, lasShoppingCart, lasBell, lasUserCircle])
+  public id: string = `1`;
+  public formGroup: any;
+  public profileImg: any;
+  
+  constructor(
+    private backendService: BackendService,
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private msg: NzMessageService
+  ) {
+    this.formGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      cep: ['', Validators.required],
+      email: ['', Validators.required],
+    })
+    this.activatedRoute.paramMap.subscribe((param: ParamMap) => {
+      this.initUpdate();
+    })
+
+    this.profileImg = "./assets/images/imagem (1).png";
+  }
+
+  public initUpdate() {
+    this.backendService.getByIdUser(this.id).subscribe((result: any) => {
+      this.formGroup.patchValue(result);
+      this.profileImg=result.file;
+    })
+  }
+
+  public submit() {
+    if (!this.formGroup.valid) {
+      alert("Inválido");
+      return;
+    }
+    this.backendService.update({
+      id: this.id,
+      ...this.formGroup.value
+    }
+    ).subscribe((result: any) => {
+      console.log(result)
+      this.router.navigate([`user`]);
+    })
+  }
+
+  handleChange(info: NzUploadChangeParam): void {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      this.msg.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      this.msg.error(`${info.file.name} file upload failed.`);
+    }
+    this.formGroup.get('file').setValue(info.file.response.data.path);
+    
   }
 }
