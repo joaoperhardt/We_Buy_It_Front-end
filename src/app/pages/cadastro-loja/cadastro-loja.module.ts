89dfbbc980329b52d3bde2a13c862556da78dee9
olvraline <142238858+olvraline@users.noreply.github.com>
2024-02-22 19:39:11 -0300

    add-cadastro-loja

---
+++ src/app/pages/cadastro-loja/cadastro-loja.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastroLojaComponent } from './cadastro-loja.component';
+
+
+
+@NgModule({
+  declarations: [
+    CadastroLojaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CadastroLojaModule { }
