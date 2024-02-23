0289a2c180fbba7ec2e74ece22974e913ce6ec36
olvraline <142238858+olvraline@users.noreply.github.com>
2024-02-23 16:20:12 -0300

    ajustes-cadastro-loja

--- src/app/pages/cadastro-loja/cadastro-loja.component.ts
+++ src/app/pages/cadastro-loja/cadastro-loja.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/backend/user.service';
-import { ActivatedRoute, Route, Router } from '@angular/router';
-import { NzUploadChangeParam, NzUploadFile } from 'ng-zorro-antd/upload';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { NzUploadComponent, NzUploadModule } from 'ng-zorro-antd/upload';
-import { STRING_TYPE } from '@angular/compiler';
+import { __param } from 'tslib';
 
 
 @Component({
@@ -14,17 +11,17 @@ import { STRING_TYPE } from '@angular/compiler';
   styleUrls: ['./cadastro-loja.component.scss']
 })
 export class CadastroLojaComponent {
-    public formGroup: any;
-    public id: string = '0';
-    public constructor(
-      private formBuilder: FormBuilder,
-      private userService: UserService,
-      private activadeRoute: ActivatedRoute,
-      private router: Router,
-      private msg: NzMessageService
+  public formGroup: any;
+  public id: string = `0`;
+getFormControlStatus: any;
+  public constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
     ){
       this.formGroup = this.formBuilder.group({
-        Name: ['', Validators.required,],
+      Name: ['', Validators.required,],
       Description: ['', Validators.required,],
       Cep: ['', Validators.required,],
       State: ['', Validators.required,],
@@ -40,30 +37,19 @@ export class CadastroLojaComponent {
       })
     
     }
-    public submit() {
+  public submit() {
+    if (!this.formGroup.valid) {
+      alert('Invalido');
+      return;
+    }
+  
+    this.userService.postcadastroloja(this.formGroup.value).subscribe((result: any) => {
+      this.router.navigate([``]);
+    });
+    
+    
+  }
+  onSubmit() {
     console.log(this.formGroup.value);
-  
-      if (!this.formGroup.valid) {
-        alert('Preencha Corretamente');
-        return;
-      }
-  
-  
-      this.userService.post(this.formGroup.value).subscribe((result: any) => {
-        this.router.navigate([``]);
-      });
-      
-    }
-  
-    handleChange(info: NzUploadChangeParam): void {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === 'done') {
-        this.msg.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        this.msg.error(`${info.file.name} file upload failed.`);
-      }
-      this.formGroup.get('file').setValue(info.file.response.data.path);    
-    }
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeaderModule } from './header/header.module';
+import { CadastroLojaModule } from "../cadastro-loja/cadastro-loja.module";
 
 
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HeaderModule
-  ],
-  exports: [
-    HomeComponent
-  ]
+    declarations: [
+        HomeComponent
+    ],
+    exports: [
+        HomeComponent
+    ],
+    imports: [
+        CommonModule,
+        HeaderModule,
+        CadastroLojaModule
+    ]
 })
 export class HomeModule { }
