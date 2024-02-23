90108975380414e21230a0250c842a459230a776
olvraline <142238858+olvraline@users.noreply.github.com>
2024-02-23 16:31:16 -0300

    correções

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeaderModule } from './header/header.module';
-import { CadastroLojaModule } from "../cadastro-loja/cadastro-loja.module";
-
-
 
 @NgModule({
     declarations: [
@@ -16,7 +13,6 @@ import { CadastroLojaModule } from "../cadastro-loja/cadastro-loja.module";
     imports: [
         CommonModule,
         HeaderModule,
-        CadastroLojaModule
     ]
 })
 export class HomeModule { }
