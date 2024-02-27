98be0362a2909681f697ba14d2d09cf2a6b3d50b
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2024-02-26 21:54:44 -0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { HeaderComponent } from './pages/home/header/header.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { ProductComponent } from './pages/product/product.component';
 import { PrincipalTelaComponent } from './pages/principal-tela/principal-tela.component';
+import { CadastroAnuncioComponent } from './pages/cadastro-anuncio/cadastro-anuncio.component';
+import { TelaPesquisaComponent } from './pages/tela-pesquisa/tela-pesquisa.component';
 
 const routes: Routes = [
   {path: 'home',  pathMatch: `full`, component: PrincipalTelaComponent},
   {path: '', pathMatch: `full`, component: PrincipalTelaComponent},
   {path: 'user', pathMatch: `full`, component: UserProfileComponent},
-  {path: 't', pathMatch: `full`, component: ProductComponent}
+  {path: 't', pathMatch: `full`, component: ProductComponent},
+  {path: 'anuncio', pathMatch: `full`, component: CadastroAnuncioComponent},
+  {path: 'search', pathMatch: `full`, component: TelaPesquisaComponent}
 ];
 
 @NgModule({
